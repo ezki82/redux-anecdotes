a32e03cc619cb9bf171088bdc99f82cef95f2e00
Esa Salminen <esa_salminen@hotmail.com>
2021-10-24 11:48:16 +0300

    6.4 tehty, 0,5h

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -21,6 +21,8 @@ const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case 'NEW ANECDOTE':
+      return [...state, action.data]
     case 'VOTE':
       const id = action.data.id
       const anecdoteToVote = state.find(n => n.id === id)
@@ -37,6 +39,17 @@ export const voteAnecdote = (id) => {
   return {
     type: 'VOTE',
     data: {id}
+  }
+}
+
+export const createAnecdote = (content) => {
+  return {
+    type: 'NEW ANECDOTE',
+    data: {
+      content: content,
+      id: getId(),
+      votes: 0
+    }
   }
 }
 

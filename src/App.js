b6c42309d78fcd0cf5f34ea20e7e174158ee4a34
Esa Salminen <esa_salminen@hotmail.com>
2021-10-24 13:19:14 +0300

    6.8 tehty, 0,5h

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import React from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
-import { useSelector, useDispatch } from 'react-redux'
-import { voteAnecdote } from './reducers/anecdoteReducer'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const vote = (id) => {
-    dispatch(voteAnecdote(id))
-  }
-
   return (
     <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <h2>anecdotes</h2>
       <AnecdoteForm />
+      <AnecdoteList />
     </div>
   )
 }
